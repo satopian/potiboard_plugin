66b6891188439af1711d84e986a29e22b9cecb9a
satopian <satopian@mia.eek.jp>
2021-02-26 12:55:37 +0900

    webp対応。スレッドが存在しない画像を表示しない。

--- newimage/newimg.php
+++ newimage/newimg.php
@@ -1,6 +1,6 @@
 <?php
 // POTI-boardの最新画像をサイトの入り口のHTMLファイルに呼び出すphp
-// newimg.php(c)さとぴあ 2020 lot.200519
+// newimg.php(c)さとぴあ 2020-2021 lot.210226
 // https://pbbs.sakura.ne.jp/
 //フリーウェアですが著作権は放棄しません。
 
@@ -11,7 +11,7 @@
 //newimg.php ←このファイルの名前をurlで指定します。
 
 //例）
-// <img src="https://hoge.ne.jp/bbs/newimg.php" alt="" width="300">
+// <img src="https://example.com/bbs/newimg.php" alt="" width="300">
 //↑
 //この例では横幅300px、高さの指定なし。
 
@@ -28,20 +28,27 @@ $default='';
 //--------- 説明と設定ここまで ---------
 
 include(__DIR__.'/config.php');//config.phpの設定を読み込む
+$tree=file(TREEFILE);
 $fp = fopen(LOGFILE, "r");//ログを開く
 	$filename='';
 	$i=0;
 	while ($line = fgets($fp ,4096)) {
-		list(,,,,,,,,,$ext,,,$time,) = explode(",", $line);
+		list($no,,,,,,,,,$ext,,,$time,) = explode(",", $line);
 		if ($ext&&is_file(IMG_DIR.$time.$ext)){
-			if(is_file(THUMB_DIR.$time.'s.jpg')){//サムネイルはあるか?
-				$filename=THUMB_DIR.$time.'s.jpg';
+			foreach($tree as $treeline){
+				$treeline=','.rtrim($treeline).',';//行の両端にコンマを追加
+				if(strpos($treeline,','.$no.',')!==false){
+					if(is_file(THUMB_DIR.$time.'s.jpg')){//サムネイルはあるか?
+						$filename=THUMB_DIR.$time.'s.jpg';
+		
+					}
+					else{//サムネイルが無かったら
+						$filename=IMG_DIR.$time.$ext;
+					}
 
+					break 2;
+				}
 			}
-			else{//サムネイルが無かったら
-				$filename=IMG_DIR.$time.$ext;
-			}
-		break;
 		}
 		if($i>=500){break;}//500発言チェックして終了
 		++$i;
@@ -63,6 +70,9 @@ switch ($img_type):
 		break;
 	case 'image/gif':
 		header('Content-Type: image/gif');
+		break;
+	case 'image/webp':
+		header('Content-Type: image/webp');
 		break;
 	default :
 		header('Content-Type: image/png');
